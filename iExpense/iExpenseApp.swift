ac8c9fdf9ebf9b7b28772f68254d34b2b8a486d4
Veselin Stefanov <veski4a92@gmail.com>
2020-10-06 07:11:54 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Veselin Stefanov on 6.10.20.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
